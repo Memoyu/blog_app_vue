045bce5da69a5748a66b6ae73f7039db003c6f50
Memoyu <mmy6076@outlook.com>
2022-03-07 22:45:20 +0800

    feat：页面标识

---
+++ src/src/layout/components/footer/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>底栏</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ElButton } from "element-plus";
+
+export default defineComponent({
+    name: "Footer",
+    components: {
+        ElButton
+    },
+    setup() {
+        return {
+            // 返回的数据
+        };
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/src/layout/components/navigation_bar/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>导航栏</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ElButton } from "element-plus";
+
+export default defineComponent({
+    name: "Navigation",
+    components: {
+        ElButton
+    },
+    setup() {
+        return {
+            // 返回的数据
+        };
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
